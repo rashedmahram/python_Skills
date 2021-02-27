dde457ed4f25c4cef0c75cc13de40cb3f47680b0
rashedmahram <70206928+rashedmahram@users.noreply.github.com>
2021-02-27 23:50:31 +0300

    add all details about the python variables

--- variables.py
+++ variables.py
@@ -1,16 +1,4 @@
-## 
-#    Variables in Python
-# ............................
-#  Simple variables:
-#          - String
-#          - Int
-#          - Float
-#          - Hex
-#          - Bool
-#          - 
-# 
-# 
-# 
-# 
-# 
-# #
+""" 
+- variables
+
+"""
