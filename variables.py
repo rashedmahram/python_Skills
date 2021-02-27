a7c8447c812a44f21673abecaba3d10638c7deb6
rashedmahram <70206928+rashedmahram@users.noreply.github.com>
2021-02-27 23:28:14 +0300

    add python file and add the variable comments

---
+++ variables.py
@@ -0,0 +1,16 @@
+## 
+#    Variables in Python
+# ............................
+#  Simple variables:
+#          - String
+#          - Int
+#          - Float
+#          - Hex
+#          - Bool
+#          - 
+# 
+# 
+# 
+# 
+# 
+# #
