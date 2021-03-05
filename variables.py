9a1f56018a6dd79a9f01b595c99cfb5a5f20b031
rashedmahram <70206928+rashedmahram@users.noreply.github.com>
2021-03-05 03:01:04 +0300

    add String and numbers variables to varible file

--- variables.py
+++ variables.py
@@ -1,5 +1,24 @@
 # Basic Variables:
 
-# 1- string 
-# 2- numbers[int,float,hex,complex,]
+# 1- string [str,char,]
+# 2- numbers[int,float,hex,complex,long]
 # 1- boolean [bool]
+
+# String
+name="Rasheed"
+first_char='R'
+print('name: {}, first letter is _{}'.format(name,first_char))
+
+
+# Numbers:
+number=8
+decimal_number=number/5 #1.6
+number2=8j
+print(type(number2)) #<class 'complex'>
+number3=float.hex(number+0.3)#0x1.099999999999ap+3
+number4=hex(number)#0x8
+number5=oct(number)#0o10
+print(number5)
+
+boolean=True # or False
+print("boolean:{} or False".format(boolean))
